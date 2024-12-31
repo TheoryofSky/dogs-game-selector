a1688641721b2e05ddec7732d136cdf177fb50e2
TheoryofSky <me@michaelkeenan.ca>
2024-12-31 03:20:53 -0500

    Update GameList.tsx

--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -4,11 +4,11 @@ interface GameListProps {
 
 export default function GameList({ games }: GameListProps) {
   return (
-    <div className="mt-6">
-      <h3 className="text-xl font-bold mb-2 text-accent">Submitted Games:</h3>
-      <ul className="list-disc list-inside space-y-2 bg-primary bg-opacity-50 p-4 rounded">
+    <div className="mt-4">
+      <h3 className="text-xl font-bold mb-2">Submitted Games:</h3>
+      <ul className="list-disc list-inside">
         {games.map((game) => (
-          <li key={game} className="text-lg">{game}</li>
+          <li key={game}>{game}</li>
         ))}
       </ul>
     </div>
